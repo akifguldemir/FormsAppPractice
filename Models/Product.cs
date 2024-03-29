8eefa933cc418767cb2225cc2a064483ac602f02
akifguldemir <akifguldemir@gmail.com>
2024-03-30 01:14:05 +0300

    validation in model

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,28 +3,35 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FormsAppPractice.Models
 {
+    // [Bind("Name","Price")]
     public class Product
     {
         [Display(Name="Urun ID")]
+        // [BindNever]
         public int Id { get; set; }
 
         [Required]
         [Display(Name="Ad")]
+        [StringLength(100)]
         public string? Name { get; set; }
 
+        [Required(ErrorMessage = "Gerekli bir alan")]
+        [Range(0,100000,ErrorMessage = "Fiyat aralığa dikkat ediniz")]
         [Display(Name="Fiyat")]
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
 
+        [Required]
         [Display(Name="Resim")]
         public string? Image { get; set; }
 
-        [Display(Name="Aktif")]
         public bool IsActive { get; set; }
 
+        [Required]
         [Display(Name="Kategori")]
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
     }
 }
