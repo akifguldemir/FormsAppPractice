47d67a04c508c306e35946cc28e7098c29cd3d9a
akifguldemir <akifguldemir@gmail.com>
2024-03-29 23:43:42 +0300

    annotation

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace FormsAppPractice.Models
 {
     public class Product
     {
+        [Display(Name="Urun ID")]
         public int Id { get; set; }
+
+        [Display(Name="Ad")]
         public string? Name { get; set; }
+
+        [Display(Name="Fiyat")]
         public decimal Price { get; set; }
+
+        [Display(Name="Resim")]
         public string? Image { get; set; }
         public bool IsActive { get; set; }
         public int CategoryId { get; set; }
